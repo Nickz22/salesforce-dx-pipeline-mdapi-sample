49e25a21c93203cbb82e17e27a42c1a4485a82be
Nicholas Zozaya <nzozaya@snapchat.com>
2019-01-07 20:17:20 -0800

    remove fixed selected permissions div

--- force-app/main/default/aura/ChoosePermissions/ChoosePermissionsController.js
+++ force-app/main/default/aura/ChoosePermissions/ChoosePermissionsController.js
@@ -36,33 +36,6 @@
 
     },
 
-    handleMouseOver : function(component, event, helper){
-
-        // to set relative top/left of div, get sibling position
-        var checkboxGroup = component.find("checkboxGroup").getElement().getBoundingClientRect();
-
-        var containerDiv = component.find("container-div").getElement().getBoundingClientRect();
-
-        // to maintain div scroll, only set positioning once
-        if(!(component.get("{!v.scrollerDivTopSet}"))){
-
-            // set scroller-div top to height of checkboxGroup
-            component.set("{!v.scrollerDivTop}", checkboxGroup.top);
-
-            // set scroller-div next to checkboxGroup
-            component.set("{!v.scrollerDivLeft}", checkboxGroup.right + ((containerDiv.right - checkboxGroup.right)/10));
-
-            // component.set("{!v.scrollerDivBottom}", checkboxGroup.bottom);
-
-            // set a sticky top height using dynamically derived coordinates
-            component.set("{!v.dynamicStyles}", "position: fixed; border: 1px solid #fe840e; border-radius: 15px;");
-
-            // ensure we don't set again
-            component.set("{!v.scrollerDivTopSet}", true);
-        }
-
-    },
-
     handleSelect : function(component, event){
 
         // get attribute
@@ -81,15 +54,15 @@
 
     launchAccessChecker : function(component, event, helper){
 
-        // get apex method
         var action = component.get("c.createAuditObjects");
         /*
         * infer context from {!v.recordId} attribute, which 
          * will be empty if check wasn't launched from
         * Access Check Configuration record page.
-         */        
-
-        // set apex params
+         *
+        * 
+         * set apex params
+        */        
         action.setParams({ 
                             selectedPermissions : component.get("{!v.chosenPerms}"),
                             configId : component.get("{!v.recordId}")
