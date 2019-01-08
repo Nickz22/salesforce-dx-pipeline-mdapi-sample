d94239ee1379832cfc57592cd701e628756feeaa
Nicholas Zozaya <nzozaya@snapchat.com>
2019-01-07 21:37:08 -0800

    refactored component code

--- force-app/main/default/aura/ChoosePermissions/ChoosePermissionsController.js
+++ force-app/main/default/aura/ChoosePermissions/ChoosePermissionsController.js
@@ -1,97 +1,73 @@
 ({
-    getPermSetFields : function(component, event, helper) {
+    /*
+   * @author          Nick Zozaya
+     * @description     dispatch helper action to set checkboxGroup values
+   * @return          Void
+     * @param         	component, helper
+	*/
+    getPermSetFields : function(component, helper) {
 
         var action = component.get("c.getPermissionSetFields");
 
         // callback actions
         action.setCallback(this, function(response) {
+
+            // get callback status
             var state = response.getState();
 
-            if (state === "SUCCESS") {
+            if(state === "SUCCESS"){
+            
+                helper.getSystemPermissions(component, response.getReturnValue());
 
-                var returnPerms = response.getReturnValue();
-
-                var optionList = [];
-
-                for(var i = 0; i < returnPerms.length; i++){
-
-                    optionList.push({"label": returnPerms[i], "value": returnPerms[i]});
-
-                }
-
-                component.set("{!v.permissions}", optionList);
-            }
-            else if (state === "ERROR") {
-                var errors = response.getError();
-                if (errors) {
-                    if (errors[0] && errors[0].message) {
-                        throw new AuraHandledException("Error message: " + 
-                        errors[0].message);
-                    }
-                }
+            }else if(state === "ERROR"){
+                
+                helper.handleGetSysPermErrors(component, response.getError());
             }
         });
+
         // queue @AuraEnabled method
         $A.enqueueAction(action);
-
     },
 
-    handleSelect : function(component, event){
+    /*
+   * @author          Nick Zozaya
+     * @description     dispatch helper action to set Selected Permissions values
+   * @return          Void
+     * @param         	component, event, helper
+	*/
+    handleSelect : function(component, event, helper){
 
-        // get attribute
-        var p = component.get("{!v.chosenPerms}");
-        
-        // apply selected checkbox values to attribute
-        p = event.getParam("value");
-
-        // set div content
-        component.set("{!v.chosenPerms}", p);
-
-        // show div
-        component.set("{!v.showSelectedPermissions}", "true");
+        helper.setSelectedPermissions(component, event);
     },
 
-
-    launchAccessChecker : function(component, event, helper){
+    /*
+   * @author          Nick Zozaya
+     * @description     launch access checker via apex method, then dispatch helper actions to handle return result
+   * @return          Void
+     * @param         	component, helper
+	*/
+    launchAccessChecker : function(component, helper){
 
         var action = component.get("c.createAuditObjects");
         /*
-        * infer context from {!v.recordId} attribute, which 
-         * will be empty if check wasn't launched from
-        * Access Check Configuration record page.
+        * infer context from {!v.recordId} attribute, which will be empty if check wasn't launched from Access_Check_Configuration__c record page.
          *
-        * 
-         * set apex params
-        */        
-        action.setParams({ 
-                            selectedPermissions : component.get("{!v.chosenPerms}"),
-                            configId : component.get("{!v.recordId}")
-                        });
+        * set apex params
+         */
+        action.setParams({ selectedPermissions : component.get("{!v.chosenPerms}"), configId : component.get("{!v.recordId}") });
 
         // callback actions
-        action.setCallback(this, function(response) {
-            
+        action.setCallback(this, function(response){
+
             var state = response.getState();
 
-            if (state === "SUCCESS") {
+            if(state === "SUCCESS"){
 
-                // process successfully initiated, fire toast
-                var resultsToast = $A.get("e.force:showToast");
-                resultsToast.setParams({
-                    "title": "Saved",
-                    "message": "Access check queued. You'll receive an email when it's complete.",
-                    "type": "SUCCESS"
-                });
-                resultsToast.fire();
-                
-            }else if (state === "ERROR") {
-                var errors = response.getError();
-                if (errors) {
-                    if (errors[0] && errors[0].message) {
-                        throw new AuraHandledException("Error message: " + 
-                        errors[0].message);
-                    }
-                }
+                helper.launchToast(component);
+
+            }else if(state === "ERROR"){
+
+                helper.handleLaunchErrors(response.getError());
             }
         });
         // queue @AuraEnabled method
--- force-app/main/default/aura/ChoosePermissions/ChoosePermissionsHelper.js
+++ force-app/main/default/aura/ChoosePermissions/ChoosePermissionsHelper.js
@@ -1,5 +1,96 @@
 ({
-    helperMethod : function() {
+    /*
+   * @author          Nick Zozaya
+     * @description     push returned PermissionSet.SystemPermissions into lightning component
+   * @return          Void
+     * @param         	component, returnPerms
+	*/
+    getSystemPermissions : function(component, returnPerms) {
 
+        // initialize container
+        var optionList = [];
+
+        // iterate over returned System Permissions
+        for(var i = 0; i < returnPerms.length; i++){
+
+            // add permission to container
+            optionList.push({ "label" : returnPerms[i], "value" : returnPerms[i]});
+
+        }
+
+        // set attribute value
+        component.set("{!v.permissions}", optionList);
+    },
+
+    /*
+   * @author          Nick Zozaya
+     * @description     throw error when apex-action returns error
+   * @return          Void
+     * @param         	errors
+	*/
+    handleGetSysPermErrors : function(errors) {
+
+        if (errors) {
+
+            if (errors[0] && errors[0].message) {
+
+                throw new AuraHandledException("Error message: " + errors[0].message);
+            }
+        }
+    },
+
+    /*
+   * @author          Nick Zozaya
+     * @description     add selected System Permission to attribute
+   * @return          Void
+     * @param         	component, event
+	*/
+    setSelectedPermissions : function(component, event){
+
+        // get attribute
+        var p = component.get("{!v.chosenPerms}");
+        
+        // apply selected checkbox values to attribute
+        p = event.getParam("value");
+
+        // set div content
+        component.set("{!v.chosenPerms}", p);
+
+        // show div
+        component.set("{!v.showSelectedPermissions}", "true");
+    },
+
+    /*
+   * @author          Nick Zozaya
+     * @description     fire toast message
+   * @return          Void
+     * @param         	
+	*/
+    launchToast : function(){
+
+        // process successfully initiated, fire toast
+        var resultsToast = $A.get("e.force:showToast");
+        resultsToast.setParams({
+            "title": "Saved",
+            "message": "Access check queued. You'll receive an email when it's complete.",
+            "type": "SUCCESS"
+        });
+
+        resultsToast.fire();
+    },
+
+    /*
+   * @author          Nick Zozaya
+     * @description     throw error when apex-action returns error
+   * @return          Void
+     * @param         	errors
+	*/
+    handleLaunchError : function(errors){
+
+        if (errors) {
+            if (errors[0] && errors[0].message) {
+                throw new AuraHandledException("Error message: " + errors[0].message);
+            }
+        }
     }
 })
